87957dbb356346c285924f35ade53f236bc09863
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-23 17:58:18 -0500

    Se agrego endpoint /explorers /explorers/:id

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+// Require para usar Prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.get('/', (req, res) => {
+  res.json({message: 'alive'});
+});
+
+app.get('/explorers', async (req, res) => {
+    const allExplorers =  await prisma.explorer.findMany({});
+    res.json(allExplorers);
+  });
+
+  app.get('/explorers/:id', async (req, res) => {
+    const id = req.params.id;
+    const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+    res.json(explorer);
+  });
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+});
