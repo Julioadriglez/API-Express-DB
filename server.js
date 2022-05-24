b1386dfdccf2fcb02b007068ae49be7be69ed025
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-24 17:59:40 -0500

    se crea endpoints get allmc, get id y  pos mc

--- server.js
+++ server.js
@@ -12,26 +12,49 @@ app.get('/', (req, res) => {
 });
 
 app.get('/explorers', async (req, res) => {
-    const allExplorers =  await prisma.explorer.findMany({});
-    res.json(allExplorers);
-  });
+  const allExplorers =  await prisma.explorer.findMany({});
+  res.json(allExplorers);
+});
 
-  app.get('/explorers/:id', async (req, res) => {
-    const id = req.params.id;
-    const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
-    res.json(explorer);
-  });
+app.get('/mc', async (req, res) => {
+  const allMc =  await prisma.MC.findMany({});
+  res.json(allMc);
+});
 
-  app.post('/explorers', async (req, res) => {
-    const explorer = {
-      name: req.body.name,
-      username: req.body.username,
-      mission: req.body.mission
-     };
-    const message = 'Explorer creado.';
-    await prisma.explorer.create({data: explorer});
-    return res.json({message});
-  });
+app.get('/explorers/:id', async (req, res) => {
+  const id = req.params.id;
+  const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+  res.json(explorer);
+});
+
+app.get('/mc/:id', async (req, res) => {
+  const id = req.params.id;
+  const mc = await prisma.MC.findUnique({where: {id: parseInt(id)}});
+  res.json(mc);
+});
+
+app.post('/explorers', async (req, res) => {
+  const explorer = {
+    name: req.body.name,
+    username: req.body.username,
+    mission: req.body.mission
+  };
+  const message = 'Explorer creado.';
+  await prisma.explorer.create({data: explorer});
+  return res.json({message});
+});
+
+app.post('/mc', async (req, res) => {
+  const mc = {
+    name: req.body.name,
+    missionCommander: req.body.missionCommander,
+    lang: req.body.lang,
+    enrollments: req.body.enrollments
+  };
+  const message = 'Explorer creado.';
+  await prisma.MC.create({data: mc});
+  return res.json({message});
+});
 
   app.put('/explorers/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
